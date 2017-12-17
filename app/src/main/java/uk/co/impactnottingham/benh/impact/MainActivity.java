dbbd389b7aa16fefe0896df42d84f02c74ade47c
benh14 <splodgemolly@gmail.com>
2017-12-17 19:03:04 +0000

    Initial commit

---
+++ app/src/main/java/uk/co/impactnottingham/benh/impact/MainActivity.java
@@ -0,0 +1,13 @@
+package uk.co.impactnottingham.benh.impact;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
