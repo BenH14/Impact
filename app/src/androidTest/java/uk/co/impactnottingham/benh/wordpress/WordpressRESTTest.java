d0db305529e91e93c5f0362cfaa789ddbb852777
BenH14 <splodgemolly@gmail.com>
2018-05-15 16:58:34 +0100

    Added test for parsing image link of specific size

--- app/src/androidTest/java/uk/co/impactnottingham/benh/wordpress/WordpressRESTTest.java
+++ app/src/androidTest/java/uk/co/impactnottingham/benh/wordpress/WordpressRESTTest.java
@@ -1,15 +1,25 @@
 package uk.co.impactnottingham.benh.wordpress;
 
+import android.support.test.InstrumentationRegistry;
+import android.util.JsonReader;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
 
 /**
  * Created by bth on 14/05/2018.
  */
 public class WordpressRESTTest {
+
+    private static final String JSON_MEDIA_FILENAME       = "media_formatted.json";
+    private static final String EXPECTED_MEDIUM_LARGE_URL = "https://impactnottingham.com/wp-content/uploads/2018/01/3322890252_1682826803_b-768x511.jpg";
+    private static final String EXPECTED_THUMBNAIL_URL    = "https://i1.wp.com/impactnottingham.com/wp-content/uploads/2018/01/3322890252_1682826803_b.jpg?resize=150%2C150&ssl=1";
 
     private WordpressREST wprest;
 
@@ -33,7 +43,26 @@ public class WordpressRESTTest {
         params = new RequestParameters();
         params.addParameter("per_page", "22");
         Assert.assertEquals(22, wprest.getArticlesList(params).size());
+    }
 
+    private JsonReader getMediaJson() throws IOException {
+        InputStream       stream       = InstrumentationRegistry.getInstrumentation().getContext().getAssets().open(JSON_MEDIA_FILENAME);
+        InputStreamReader streamReader = new InputStreamReader(stream, "UTF-8");
+        return new JsonReader(streamReader);
+    }
+
+    @Test
+    public void testParsingImageLink() throws IOException {
+        JsonReader mediaJson = getMediaJson();
+
+        Assert.assertEquals(new URL(EXPECTED_MEDIUM_LARGE_URL), new WordpressREST().getImageLink(mediaJson, WordpressREST.IMAGE_SIZE_MEDIUM_LARGE));
+
+        mediaJson.close();
+        mediaJson = getMediaJson();
+
+        Assert.assertEquals(new URL(EXPECTED_THUMBNAIL_URL), new WordpressREST().getImageLink(mediaJson, WordpressREST.IMAGE_SIZE_THUMBNAIL));
+
+        mediaJson.close();
     }
 
 }
