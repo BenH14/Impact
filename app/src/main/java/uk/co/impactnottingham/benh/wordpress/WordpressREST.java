0b13a4c00c3ca92b95e135c0f5598f87f3808894
BenH14 <splodgemolly@gmail.com>
2018-05-15 23:30:07 +0100

    Getting image link added

--- app/src/main/java/uk/co/impactnottingham/benh/wordpress/WordpressREST.java
+++ app/src/main/java/uk/co/impactnottingham/benh/wordpress/WordpressREST.java
@@ -1,5 +1,6 @@
 package uk.co.impactnottingham.benh.wordpress;
 
+import android.media.Image;
 import android.util.JsonReader;
 import android.util.Log;
 
@@ -19,9 +20,16 @@ public class WordpressREST {
 
     private static final String TAG = WordpressREST.class.getName();
 
-
     //CONSTANTS
     private static final String POSTS_ENDPOINT_ADDRESS = "https://impactnottingham.com/wp-json/wp/v2/posts";
+    private static final String MEDIA_ENDPOINT_ADDRESS = "https://impactnottingham.com/wp-json/wp/v2/media/";
+
+    public static final int IMAGE_SIZE_FULL         = 0;
+    public static final int IMAGE_SIZE_LARGE        = 1;
+    public static final int IMAGE_SIZE_MEDIUM_LARGE = 2;
+    public static final int IMAGE_SIZE_MEDIUM       = 3;
+    public static final int IMAGE_SIZE_THUMBNAIL    = 4;
+
 
     private List<Article> parseArticles(JsonReader json) throws IOException {
 
@@ -37,8 +45,57 @@ public class WordpressREST {
         return articles;
     }
 
-    private JsonReader getPostsJson( RequestParameters params) throws IOException {
+    private JsonReader getPostsJson(RequestParameters params) throws IOException {
         return getJson(new URL(POSTS_ENDPOINT_ADDRESS), params);
+    }
+
+    JsonReader getMediaJson(int postID, RequestParameters params) throws IOException {
+        return getJson(new URL(MEDIA_ENDPOINT_ADDRESS + String.valueOf(postID)), params);
+    }
+
+    URL getImageLink(JsonReader json, int size) throws IOException {
+
+        URL url = null;
+        String sizeName;
+
+        switch (size) {
+            case IMAGE_SIZE_FULL:
+                sizeName = "full";
+                break;
+            case IMAGE_SIZE_LARGE:
+                sizeName = "large";
+                break;
+            case IMAGE_SIZE_MEDIUM_LARGE:
+                sizeName = "medium_large";
+                break;
+            case IMAGE_SIZE_MEDIUM:
+                sizeName = "medium";
+                break;
+            case IMAGE_SIZE_THUMBNAIL:
+                sizeName = "thumbnail";
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid image size requested, use the constants");
+        }
+
+        json.beginObject();
+
+        while (json.hasNext()) {
+            String name = json.nextName();
+            if (name.equals("media_details") || name.equals("sizes") || name.equals(sizeName)) {
+                json.beginObject();
+            } else if (name.equals("source_url")) {
+                url = new URL(json.nextString());
+            } else {
+                json.skipValue();
+            }
+        }
+
+        return url;
+    }
+
+    Image getImage(JsonReader json) {
+        return null; //todo
     }
 
     private JsonReader getJson(URL rootURL, RequestParameters params) throws IOException {
