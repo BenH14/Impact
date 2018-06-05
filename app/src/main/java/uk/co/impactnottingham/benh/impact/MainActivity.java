6553fc75a8245358e70de1048e7dc9137e1ad6da
BenH14 <splodgemolly@gmail.com>
2018-06-05 19:28:48 +0100

    Sidebar added

--- app/src/main/java/uk/co/impactnottingham/benh/impact/MainActivity.java
+++ app/src/main/java/uk/co/impactnottingham/benh/impact/MainActivity.java
@@ -2,11 +2,15 @@ package uk.co.impactnottingham.benh.impact;
 
 import android.os.Bundle;
 import android.support.annotation.UiThread;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.*;
 import uk.co.impactnottingham.benh.glide.GlideApp;
@@ -21,12 +25,12 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG                      = "MainActivity";
     private static final int    ARTICLES_PER_REQUEST     = 10;
-    private static final int    SCROLL_LOAD_OFFSET       = 1;
+    private static final int    SCROLL_LOAD_OFFSET       = 3;
 
     private final List<Article> articles;
 
-    private RelativeLayout rootLayout;
     private RecyclerView mRecyclerView;
+    private DrawerLayout mDrawer;
 
     private int mPage;
 
@@ -40,13 +44,14 @@ public class MainActivity extends AppCompatActivity {
         loading = new AtomicReference<>(false);
     }
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        rootLayout = findViewById(R.id.activity_main);
         mRecyclerView = findViewById(R.id.recycler_headlines);
+        mDrawer = findViewById(R.id.drawer_layout);
 
         mAdapter = new HeadlineAdapter(new ArrayList<>());
 
@@ -58,22 +63,37 @@ public class MainActivity extends AppCompatActivity {
             public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
                 super.onScrolled(recyclerView, dx, dy);
 
-                int itemCount = layoutManager.getItemCount();
-                int lastVisibleItem = layoutManager.findLastVisibleItemPosition();
+                if (dy > 0) {  // Only look if we're scrolling down
+                    int itemCount       = layoutManager.getItemCount();
+                    int lastVisibleItem = layoutManager.findLastVisibleItemPosition();
 
-                Log.d(TAG, "onScrolled: itemCount = " + itemCount + "     last visible = " + lastVisibleItem);
+//                    Log.d(TAG, "onScrolled: itemCount = " + itemCount + "     last visible = " + lastVisibleItem);
 
-                if(itemCount - SCROLL_LOAD_OFFSET <= lastVisibleItem) {
-                    loadArticles();
+                    if (itemCount - SCROLL_LOAD_OFFSET <= lastVisibleItem) {
+                        loadArticles();
+                    }
                 }
             }
         });
 
         Toolbar mainToolbar = findViewById(R.id.toolbar_main);
         setSupportActionBar(mainToolbar);
-        mainToolbar.setTitle("Toolbar Title");
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_24dp);
 
         loadArticles();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawer.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void loadArticles() {
@@ -98,26 +118,5 @@ public class MainActivity extends AppCompatActivity {
                 mAdapter.add(a);
             }
         });
-    }
-
-    @UiThread
-    private void addHeadline(Article article) {
-        View headline = getLayoutInflater().inflate(R.layout.headline_list_row, findViewById(R.id.activity_main), false);
-        Log.d(TAG, "addHeadline: inflating headlines view");
-
-        TextView title   = headline.findViewById(R.id.headline_title);
-        TextView excerpt = headline.findViewById(R.id.headline_excerpt);
-
-        title.setText(article.getTitle());
-        excerpt.setText(article.getSnippet());
-        rootLayout.addView(headline);
-
-        new GetImageLinkTask(WordpressREST.IMAGE_SIZE_THUMBNAIL, (URL url) -> runOnUiThread(() ->
-                GlideApp.with(this)
-                        .asDrawable()
-                        .load(url.toString())
-                        .thumbnail(0.1f)
-                        .into((ImageView) headline.findViewById(R.id.headline_image)))
-        ).execute(article.getImageId());
     }
 }
